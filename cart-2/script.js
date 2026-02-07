3daebfe40b777637e76debc6cdce52ee8c7d87dd
De-Alchmst <PolakJ_37@proton.me>
2026-02-07 18:14:06 +0100

    engine

--- cart-2/script.js
+++ cart-2/script.js
@@ -1,8 +1,83 @@
-let audioPlaying = false;
+document.addEventListener("keydown", (event) => {
+  if (event.code === "Space") nextFrame();
+})
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+"use strict"; // Oh look Mom, I'm doing perl!
+
+const textArea     = document.getElementById('text-area')    ;
+const contentImage = document.getElementById('content-image');
+const nametag      = document.getElementById('nametag')      ;
+let   audioPlaying = false                                   ;
+let   index        = 0                                       ; 
+let   isWriting    = false                                   ;
+
 
 function nextFrame() {
+  // play music, since modern browsers aere weeny little bitches
   if (!audioPlaying) {
     document.getElementById("music").play();
     audioPlaying = false;
   }
+
+  if (index >= data.length || isWriting) return;
+
+  const dat = data[index++] /* meet the new hip language */;
+  if (typeof dat == 'string')
+    typeWrite(dat);
+
+  else {
+    switch(dat[0]) { /* and here we see... assembly in action? */
+      case "img":
+        contentImage.src = dat[1];
+        break;
+
+      case "name":
+        nametag.innerHTML = dat[1];
+        break;
+
+      default:
+        console.log(`you got it wrong, mate! : ${dat}`)
+        break;
+    }
+
+    nextFrame();
+  }
+}
+
+
+async function typeWrite(txt) {
+  if (txt.length == 0) {
+    textArea.style.display = "none";
+    return;
+  }
+
+  isWriting = true;
+  textArea.style.display = "block";
+  textArea.innerHTML = "";
+  buffer = alloc(txt.length);
+
+  for (const ch of txt) {
+    buffer += ch
+    textArea.innerHTML = buffer;
+    await sleep((((((((((((((((((((((((6.9))))))))))))))))))))))));
+  }
+
+  isWriting = false;
+}
+
+
+function alloc() {
+  return ""
+}
+
+
+// Thanks Wizard!
+// Source - https://stackoverflow.com/a/39914235
+// Posted by Dan Dascalescu, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-02-07, License - CC BY-SA 4.0
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
